cc18396b461789f32e0ee41682b5b105fffae3d0
Pierre Roudier <pierre.roudier@gmail.com>
2012-01-10 15:16:45 +1300

    typo

--- clhs/R/clhs-sp.R
+++ clhs/R/clhs-sp.R
@@ -5,7 +5,7 @@ clhs.SpatialPointsDataFrame <- function(
 
   spl <- clhs(x = x@data, ...)
 
-  if (is(spl), "cLHS_result")
+  if (is(spl, "cLHS_result"))
     spl$initial_object <- x # replacing the data.frame by the SPDF object
   else
     res <- x[spl, ]
