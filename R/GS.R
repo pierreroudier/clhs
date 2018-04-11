7a9ccb98bf303a7827eba240155c8f8e8e506454
Pierre Roudier <pierre.roudier@gmail.com>
2018-04-12 11:40:32 +1200

    More refactoring/simplification of code

--- R/GS.R
+++ R/GS.R
@@ -24,59 +24,67 @@
 #' 
 GS <- function(rstack, samps, buffer, fac = NA) {
 
-    # Iterate over every point. This keeps memory usage small
+  # Iterate over every point
+  # res <- plyr::alply(coordinates(samps), function(coords) {
+  #   
+  #   # Extract all cells within x m of the sampling points. 
+  #   buff <- extract(x = rstack, y = coords, buffer = buffer, cellnumbers = TRUE, method = 'simple', df  =TRUE)
+  #   
+  #   # Apply Gower's similarity to each element of the list of extracted raster values.
+  #   # Get the cell numbers from each sample point to identity the right column in the similarity matrix. 
+  #   cellNum <- extract(x = rstack, y = coords, method = 'simple', cellnumbers = TRUE, small = TRUE)
+  #   cellnum <- cellNum[, 1] #just need the cell numbers. 
+  #   
+  # })
+  
+  # Iterate over every point. This keeps memory usage small
   f.list <- list()
   
   for(i in 1:nrow(samps)){
     
-    #2. Extract all cells within x m of the sampling points. 
-    buff <- extract(x = rstack, y = samps[i, ], buffer = buffer, cellnumbers = TRUE, method = 'simple', df  =TRUE)
+    # 2. Extract all cells within x m of the sampling points. 
+    buff_data <- extract(x = rstack, y = samps[i, ], buffer = buffer, cellnumbers = TRUE, method = 'simple', df = TRUE)
     
-    # 3.Apply gowers similarity index to each element of list of extracted raster values.
+    # 3. Apply gowers similarity index to each element of list of extracted raster values
+    
     # Get the cell numbers from each sample point to identity the right column in the similarity matrix. 
-    cellNum <- extract(x = rstack, y = samps[i,], method = 'simple', cellnumbers = TRUE, small = TRUE)
-    cellnum <- cellNum[, 1] #just need the cell numbers. 
+    cellnum <- cellFromXY(rstack, samps[i,])
     
-    # 3.b Function to calculate Gower's similarity index around each point. I used Gower's because it can handle categorical covariates, but there are other options. 
+    # 3.b Calculate Gower's similarity index around each point. 
+    #   I used Gower's because it can handle categorical covariates, 
+    #   but there could be other options. 
     
-    # First convert each list element in buff to a data frame, then remove NA values. If values are on edges this makes the elements of similarL not the same so they can't be rbind-ed together, third Force any factors to be a factor (+1 because the cell numbers are added as the first column)
-    tL <- data.frame(buff) 
-    tL <- tL[complete.cases(tL),]
+    # Only retain cases without NA values
+    buff_data <- data.frame(buff_data[complete.cases(buff_data), ])
     
-    # Warnings are suppressed because if fac is > 1 the is.na function warns that only the first value will be used.
+    # Warnings are suppressed because if fac is > 1 the is.na function 
+    # warns that only the first value will be used.
     # This does not affect the output whatsoever.   
     # All other elements of the result list are as expected.
     suppressWarnings(
-      if (is.na(fac)) {
-        tL <- tL
-      } else {
-        tL[,fac+1] <- lapply(tL[fac + 1], factor)
-      })
+      if (!is.na(fac)) {
+        buff_data[, fac + 1] <- lapply(buff_data[fac + 1], factor)
+      }
+    )
     
     # Calculate gowers similarity index and make dissimilarity object a matrix
-    GL <- daisy(x = tL[, -1], metric = 'gower')
+    gower_dissim <- daisy(x = buff_data[, names(rstack)], metric = 'gower')
+    gower_dissim <- cbind(buff_data$cell, as.matrix(gower_dissim)) 
     
-    gmatL <- cbind(tL$cell, as.matrix(GL)) 
-    
-    # Select the row of similarity indices with cell number equal to the cell number of the sample point and convert dissimilarity to similarity by subtracting from 1.  
-    finalgL <- 1 - gmatL[gmatL[, 1] == cellnum,] 
+    # Select the row of similarity indices with cell number equal to the cell number of the 
+    # sample point and convert dissimilarity to similarity by subtracting from 1.  
+    gower_sim <- 1 - gower_dissim[gower_dissim[, 1] == cellnum, ] 
     
     # Combine the cellnumbers of the raster to the similarity index. 
-    similarL <- list()
-    similarL[[1]] <- cbind(tL$cell, finalgL[-1]) 
+    s.df <- data.frame(cellnum = buff_data$cell, similarity = gower_sim[-1])
     
-    # Note this may return several warning messages from the daisy package. See https://stat.ethz.ch/R-manual/R-devel/library/cluster/html/daisy.html for more information on these warnings.
-    
-    # Convert list of cell numbers and similarity values to a dataframe
-    s.df <- do.call(rbind.data.frame, similarL) 
-    names(s.df) <- c('CellNum', 'Similar') #Give better names
-    
-    # Define a raster to hold the similarity values by selecting the first layer of the raster stack, and make all cells NA
+    # Define the raster layer storing the results
     r <- raster(rstack, layer = 0) 
     
     # Index the original raster by the cell numbers and replace the NA values with the similarity values. 
     # This results in a raster with similarity values in the buffers around each point and NA everywhere else. 
-    r[s.df$CellNum] <- s.df$Similar
+    r[s.df$cellnum] <- s.df$similarity
+    
     names(r) <- paste0('SimilarityIndex_', i)
     
     f.list[[i]] <- r
