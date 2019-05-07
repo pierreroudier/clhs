95a591581fe4a07bd614bf7918b9f4c84e0ee0e9
Pierre Roudier <pierre.roudier@gmail.com>
2019-05-07 16:40:54 +1200

    First go at refactoring to support categorical data better

--- R/clhs-data.frame.R
+++ R/clhs-data.frame.R
@@ -83,7 +83,10 @@ clhs.data.frame <- function(
   # Detection of any factor data
   i_factor <- which(!sapply(x, is.numeric))
   n_factor <- length(i_factor)
-  if (n_factor > 0) {
+  n_continuous <- ncol(x) - n_factor
+    
+  # If there is a mix of continuous and categorical data
+  if (n_factor > 0 & n_continuous > 0) { 
     data_continuous <- x[, -1*i_factor, drop = FALSE]
     data_factor <- x[, i_factor, drop = FALSE]
     # Creating a list storing the levels of each factor
@@ -91,24 +94,34 @@ clhs.data.frame <- function(
       ifelse(is.factor(x), res <- levels(x), res <- levels(factor(x)))
       res}
     )
-  } else {
+  } else if (n_factor == 0 & n_continuous > 0) {
+    # Continuous data but no categorical data
     data_continuous <- x
+  } else if (n_factor > 0 & n_continuous == 0) {
+    # categorical data but no continuous data
+    data_factor <- x
+  } else {
+    stop("Wrong types in input data.frame.")
   }
   
   metropolis <- exp(-1*0/temp) # Initial Metropolis value
-  n_data <- nrow(data_continuous) # Number of individuals in the data set
+  n_data <- nrow(x) # Number of individuals in the data set
   
-  # Edge of the strata
-  continuous_strata <- apply(
-    data_continuous, 
-    2, 
-    function(x) {
-      quantile(x, probs = seq(0, 1, length.out = size + 1), na.rm = TRUE)
-    }
-  )
-  
-  # Data correlation
-  cor_mat <- cor(data_continuous, use = "complete.obs")
+  if (n_continuous > 0) {
+    # Edge of the strata
+    continuous_strata <- apply(
+      data_continuous, 
+      2, 
+      function(x) {
+        quantile(x, probs = seq(0, 1, length.out = size + 1), na.rm = TRUE)
+      }
+    )
+    
+    # Data correlation
+    cor_mat <- cor(data_continuous, use = "complete.obs")
+  } else {
+    continuous_strata <- cor_mat <- NULL
+  }
   
   # For object/class variable
   if (n_factor == 0) data_factor_sampled <- data.frame()
@@ -122,15 +135,33 @@ clhs.data.frame <- function(
   n_remainings <- n_data - size # number of individuals remaining unsampled
   i_sampled <- c(sample(not_included, size = sampled_size, replace = FALSE), include) # individuals randomly chosen
   i_unsampled <- setdiff(1:n_data, i_sampled) # individuals remaining unsampled
-  data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE] # sampled continuous data
   
-  if (n_factor > 0) data_factor_sampled <- data_factor[i_sampled, , drop = FALSE] # sampled factor data
+  if (n_continuous > 0) {
+    data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE] # sampled continuous data
+  } else {
+    data_continuous_sampled <- data.frame()
+  }
+  if (n_factor > 0) {
+    data_factor_sampled <- data_factor[i_sampled, , drop = FALSE] # sampled factor data
+  } else {
+    data_factor_sampled <- data.frame()
+  }
   
   # objective function
-  res <- .lhs_obj(size = size, data_continuous_sampled = data_continuous_sampled, data_factor_sampled = data_factor_sampled, continuous_strata = continuous_strata, cor_mat = cor_mat, factor_obj = factor_obj, weights = weights, eta = eta)
+  res <- .lhs_obj(
+    size = size, 
+    data_continuous_sampled = data_continuous_sampled, 
+    data_factor_sampled = data_factor_sampled, 
+    continuous_strata = continuous_strata, 
+    cor_mat = cor_mat, 
+    factor_obj = factor_obj, 
+    weights = weights, 
+    eta = eta
+  )
   
   obj <- res$obj # value of the objective function
   delta_obj_continuous <- res$delta_obj_continuous
+  delta_obj_factor <- res$delta_obj_factor
   
   if (cost_mode) {
     # (initial) operational cost
@@ -144,11 +175,10 @@ clhs.data.frame <- function(
   
   # progress bar
   if (progress) pb <- txtProgressBar(min = 1, max = iter, style = 3)
-  
-  if (any(duplicated(i_sampled))) browser() 
-  if (any(i_sampled %in% i_unsampled)) browser()
+  flip <- -1
   
   for (i in 1:iter) {
+    print(paste0(i, " | ", flip < 0.5, "|", length(i_sampled)))
     
     # storing previous values
     previous <- list()
@@ -156,56 +186,118 @@ clhs.data.frame <- function(
     previous$i_sampled <- i_sampled
     previous$i_unsampled <- i_unsampled
     previous$delta_obj_continuous <- delta_obj_continuous
+    previous$delta_obj_factor <- delta_obj_factor
     
     if (cost_mode) previous$op_cost <- op_cost
     
-    if (runif(1) < 0.5) {
-      # pick a random sampled point and random unsampled point and swap them
-      idx_removed <- sample(1:length(setdiff(i_sampled, include)), size = 1, replace = FALSE)
-      spl_removed <- setdiff(i_sampled, include)[idx_removed]
-      idx_added <- sample(1:length(i_unsampled), size = 1, replace = FALSE)
-      i_sampled <- setdiff(i_sampled, include)[-idx_removed]
-      i_sampled <- c(i_sampled, i_unsampled[idx_added], include)
-      i_unsampled <- i_unsampled[-idx_added]
+    flip <- runif(1)
+    
+    if (flip < 0.5) {
+      # pick a random sampled point and random unsampled point
+      # 
+      rm_candidates <- setdiff(i_sampled, include)
+      spl_removed <- sample(rm_candidates, size = 1, replace = FALSE) 
+      spl_added <- sample(i_unsampled, size = 1, replace = FALSE)
+      
+      # swap the added and removed points from i_sampled and i_unsampled
+      #
+      
+      # 1. remove sample from i_sampled 
+      i_sampled <- setdiff(i_sampled, c(include, spl_removed))
+      
+      # 2. replace it by new sampled point 
+      i_sampled <- c(i_sampled, spl_added, include)
+      
+      # 3. remove the added sampled point from the pool of unsampled points
+      i_unsampled <- setdiff(i_unsampled, spl_added)
+      
+      # 4. add the removed sampled back into pool of unsampled points
       i_unsampled <- c(i_unsampled, spl_removed)
       
       if (any(duplicated(i_sampled))) browser() 
       if (any(i_sampled %in% i_unsampled)) browser()
       
       # creating new data sampled
-      data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
-      
+      if (n_continuous > 0) data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
       if (n_factor > 0) data_factor_sampled <- data_factor[i_sampled, , drop = FALSE]
+      
     }
     else {
+      
+      browser()
+      
+      # Compute objective function for each candidate
+      individual_obj <- weights[[1]] * res$delta_obj_continuous + 
+        weights[[2]] * res$delta_obj_factor + 
+        weights[[3]] * res$delta_obj_cor
+      
       # remove the worse sampled & resample
-      worse <- max(delta_obj_continuous[!i_sampled %in% include])
-      i_worse <- which(delta_obj_continuous[!i_sampled %in% include] == worse)
+      
+      # worse <- max(delta_obj_continuous[!i_sampled %in% include])
+      # i_worse <- which(delta_obj_continuous[!i_sampled %in% include] == worse)
+      # 
+      worse <- max(individual_obj[!i_sampled %in% include])
+      i_worse <- which(individual_obj[!i_sampled %in% include] == worse)
+      
       # If there's more than one worse candidate, we pick one at random
       if (length(i_worse) > 1) i_worse <- sample(i_worse, size = 1)
       
       # swap with reservoir
-      spl_removed <- setdiff(i_sampled, include)[i_worse] # will be removed from the sampled set. 
-      idx_added <- sample(1:n_remainings, size = 1, replace = FALSE) # new candidate that will take their place
-      i_sampled <- setdiff(i_sampled, include)[-i_worse]
-      i_sampled <- c(i_sampled, i_unsampled[idx_added], include)
-      i_unsampled <- i_unsampled[-idx_added]
+      
+      spl_removed <- setdiff(i_sampled, include)[i_worse]
+      spl_added <- sample(i_unsampled, size = 1, replace = FALSE)
+      
+      # spl_removed <- setdiff(i_sampled, include)[i_worse] # will be removed from the sampled set.
+      # idx_added <- sample(1:n_remainings, size = 1, replace = FALSE) # new candidate that will take their place
+      # i_sampled <- setdiff(i_sampled, include)[-i_worse]
+      
+      # if (any(is.na(i_sampled))) browser()
+      # 
+      # i_sampled <- c(i_sampled, i_unsampled[idx_added], include)
+      # i_unsampled <- i_unsampled[-idx_added]
+      # i_unsampled <- c(i_unsampled, spl_removed)
+      
+      # swap the added and removed points from i_sampled and i_unsampled
+      #
+      
+      # 1. remove sample from i_sampled 
+      i_sampled <- setdiff(i_sampled, c(include, spl_removed))
+      
+      # 2. replace it by new sampled point 
+      i_sampled <- c(i_sampled, spl_added, include)
+      
+      # 3. remove the added sampled point from the pool of unsampled points
+      i_unsampled <- setdiff(i_unsampled, spl_added)
+      
+      # 4. add the removed sampled back into pool of unsampled points
       i_unsampled <- c(i_unsampled, spl_removed)
       
-      if (any(duplicated(i_sampled))) browser()
       if (any(i_sampled %in% i_unsampled)) browser()
       
-      # creating new data sampled
-      data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
+      if (any(duplicated(i_sampled))) stop("duplicated point(s) in samples")
+      if (any(i_sampled %in% i_unsampled)) stop("point(s) in both sampled and unsampled datasets")
       
+      # creating new data sampled
+      if (n_continuous > 0) data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
       if (n_factor > 0) data_factor_sampled <- data_factor[i_sampled, , drop = FALSE]
     }
     
     # calc obj
-    res <- .lhs_obj(size = size, data_continuous_sampled = data_continuous_sampled, data_factor_sampled = data_factor_sampled, continuous_strata = continuous_strata, cor_mat = cor_mat, factor_obj = factor_obj, weights = weights, eta = eta)
+    res <- .lhs_obj(
+      size = size, 
+      data_continuous_sampled = data_continuous_sampled, 
+      data_factor_sampled = data_factor_sampled, 
+      continuous_strata = continuous_strata, 
+      cor_mat = cor_mat, 
+      factor_obj = factor_obj, 
+      weights = weights, 
+      eta = eta
+    )
     
     obj <- res$obj
     delta_obj_continuous <- res$delta_obj_continuous
+    delta_obj_factor <- res$delta_obj_factor
+    
     # Compare with previous iterations
     delta_obj <- obj - previous$obj
     metropolis <- exp(-1*delta_obj/temp) #+ runif(1)*temp
@@ -239,12 +331,13 @@ clhs.data.frame <- function(
     if (delta_obj > 0 & runif(1) >= metropolis | runif(1) >= metropolis_cost) {
       i_sampled <- previous$i_sampled
       i_unsampled <- previous$i_unsampled
-      data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
       
+      if (n_continuous > 0) data_continuous_sampled <- data_continuous[i_sampled, , drop = FALSE]
       if (n_factor > 0) data_factor_sampled <- data_factor[i_sampled, , drop = FALSE]
       
       obj <- previous$obj
       delta_obj_continuous <- previous$delta_obj_continuous
+      delta_obj_factor <- previous$delta_obj_factor
       
       if (cost_mode) op_cost <- previous$op_cost
     }
@@ -264,11 +357,17 @@ clhs.data.frame <- function(
   # Close progress bar
   if (progress) close(pb)
   
-  if (n_factor > 0) {
+  if (n_factor > 0 & n_continuous > 0) {
     sampled_data <- data.frame(data_continuous_sampled, data_factor_sampled)
     # reordering cols
     sampled_data <- sampled_data[, names(x)]
-  } else sampled_data <- data_continuous_sampled
+  } else if(n_factor == 0 & n_continuous > 0) {
+    sampled_data <- data_continuous_sampled 
+  } else if(n_factor > 0 & n_continuous == 0) {
+    sampled_data <- data_factor_sampled 
+  } else {
+    stop("Wrong types in input data.frame.")
+  }
   
   # Simple output - just the sampled object
   if (simple) res <- i_sampled
--- R/clhs-internal.R
+++ R/clhs-internal.R
@@ -21,38 +21,79 @@
   #
   n_cont_variables <- ncol(data_continuous_sampled)
 
-  cont_data_strata <- lapply(1:n_cont_variables, function(i) list(data_continuous_sampled[, i, drop = TRUE], continuous_strata[, i, drop = TRUE]) )
-  cont_obj_sampled <- lapply(cont_data_strata, function(x) hist(x[[1]], breaks = x[[2]], plot = FALSE)$counts)
-  cont_obj_sampled <- matrix(unlist(cont_obj_sampled), ncol = n_cont_variables, byrow = FALSE)
-
-  delta_obj_continuous <- rowSums(abs(cont_obj_sampled - eta))
-
+  if (n_cont_variables > 0) {
+    
+    cont_data_strata <- lapply(
+      1:n_cont_variables, 
+      function(i) list(data_continuous_sampled[, i, drop = TRUE], continuous_strata[, i, drop = TRUE])
+    )
+    
+    cont_obj_sampled <- lapply(
+      cont_data_strata, 
+      function(x) hist(x[[1]], breaks = x[[2]], plot = FALSE)$counts
+    )
+    
+    cont_obj_sampled <- matrix(unlist(cont_obj_sampled), ncol = n_cont_variables, byrow = FALSE)
+  
+    delta_obj_continuous <- rowSums(abs(cont_obj_sampled - eta))
+    
+    # Correlation of continuous data
+    #
+    cor_sampled <- suppressWarnings(cor(data_continuous_sampled))
+    
+    # when there's only one observation, cor() throws NAs - we set these to 1
+    cor_sampled[is.na(cor_sampled)] <- 1 
+    
+    delta_obj_cor <- sum(abs(cor_mat - cor_sampled))
+  
+    } else {
+    delta_obj_continuous <- 0
+    delta_obj_cor <- 0
+  }
+  
   # Factor variables
   #
   n_factor_variables <- ncol(data_factor_sampled)
-  if (n_factor_variables > 0) {
-    factor_obj_sampled <- lapply(1:n_factor_variables, function(x) table(data_factor_sampled[, x])/nrow(data_factor_sampled))
-    delta_obj_factor <- lapply(1:n_factor_variables, function(x) sum(abs(factor_obj_sampled[[x]] - factor_obj[[x]])))
-
-    delta_obj_factor <- unlist(delta_obj_factor)#/length(delta_obj_factor) # do we need to ponder w/ the number of factors?
-  }
-  else
-    delta_obj_factor <- 0
-
-  # Correlation of continuous data
-  #
-  cor_sampled <- suppressWarnings(cor(data_continuous_sampled))
   
-  # when there's only one observation, cor() throws NAs - we set these to 1
-  cor_sampled[is.na(cor_sampled)] <- 1 
+  if (n_factor_variables > 0) {
+    factor_obj_sampled <- lapply(
+      1:n_factor_variables, 
+      function(x) {
+        # Remove non-populated factor levels
+        if (is.factor(data_factor_sampled[, x])) {
+          data_factor_sampled[, x] <- droplevels(data_factor_sampled[, x])
+        }
+        
+        res <- table(data_factor_sampled[, x])/nrow(data_factor_sampled)
+        
+        res
+      }
+    )
+    
+    delta_obj_factor <- lapply(
+      1:n_factor_variables, 
+      function(x) sum(abs(factor_obj_sampled[[x]] - factor_obj[[x]]))
+    )
 
-  delta_obj_cor <- sum(abs(cor_mat - cor_sampled))
+    # delta_obj_factor <- unlist(delta_obj_factor)#/length(delta_obj_factor) # do we need to ponder w/ the number of factors?
+    delta_obj_factor <- abs( unlist(factor_obj_sampled) - unlist(factor_obj) )
+    
+  } else {
+    delta_obj_factor <- 0
+  }
 
   # Objective function
   #
-  obj <- weights[[1]]*sum(delta_obj_continuous) + weights[[2]]*sum(delta_obj_factor) + weights[[3]]*delta_obj_cor
+  obj <- weights[[1]] * sum(delta_obj_continuous) + 
+    weights[[2]] * sum(delta_obj_factor) + 
+    weights[[3]] * delta_obj_cor
 
   # Returning results
   #
-  list(obj = obj, delta_obj_continuous = delta_obj_continuous, delta_obj_factor = delta_obj_factor, delta_obj_cor = delta_obj_cor)
+  list(
+    obj = obj, 
+    delta_obj_continuous = delta_obj_continuous, 
+    delta_obj_factor = delta_obj_factor, 
+    delta_obj_cor = delta_obj_cor
+  )
 }
