e0a5b19383bbc776c7638c5d75a2202b8b246758
Pierre Roudier <pierre.roudier@gmail.com>
2021-05-06 14:36:03 +1200

    removed example from the clhs-package page

--- R/clhs-package.R
+++ R/clhs-package.R
@@ -43,16 +43,6 @@
 #' 
 #' @keywords sampling 
 #' 
-#' @examples
-#' 
-#' df <- data.frame(
-#'   a = runif(1000), 
-#'   b = rnorm(1000), 
-#'   c = sample(LETTERS[1:5], size = 1000, replace = TRUE)
-#' )
-#' res <- clhs(df, size = 50, iter = 2000, progress = FALSE)
-#' str(res)
-#' 
 NULL
 
 #' Conditioned Latin Hypercube Sampling result
